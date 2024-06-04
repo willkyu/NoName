4c33f9599c810a11af54717fae17bc69d06647e9
willkyu <z496373158@163.com>
2024-06-04 17:10:57 +0800

    Add some features.

--- plugin/test.py
+++ plugin/test.py
@@ -4,16 +4,24 @@ from sim.battle import *
 print("Done.")
 
 
-commandPriorityDict = {"a": (3, 5), "b": (2, 7), "c": (3, 2), "d": (1, 6)}
+commandPriorityDict = {"a": 3, "b": 2, "c": 3, "d": 1}
+# print(
+#     [
+#         key
+#         for key, value in sorted(
+#             commandPriorityDict.items(), key=lambda x: (-x[1][0], -x[1][1])
+#         )
+#     ]
+# )
+
 print(
     [
         key
-        for key, value in sorted(
-            commandPriorityDict.items(), key=lambda x: (-x[1][0], -x[1][1])
+        for key in sorted(
+            commandPriorityDict.keys(), key=lambda x: (-commandPriorityDict[x])
         )
     ]
 )
 
-
-test = getNickname("403363659")
-print(test)
+# test = getNickname("403363659")
+# print(test)
