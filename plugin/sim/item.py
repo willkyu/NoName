68c49254402fc771c8081b38bde8cb01a418178c
xunx911 <550461802@qq.com>
2024-06-08 18:03:30 +0800

    重写少量代码测试

--- plugin/sim/item.py
+++ plugin/sim/item.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import Enum
 from typing import Callable
 
 from .non_events import NonEventsObj
@@ -6,8 +7,14 @@ from .non_events import NonEventsObj
 SpecialEffectClass = Callable | str | None  # 特殊效果，还没想好是函数还是字符串
 
 
-@dataclass
-class Rarity:
+# @dataclass
+# class Rarity():
+#     WHITE = 0.5
+#     BLUE = 0.3
+#     PURPLE = 0.15
+#     GOLD = 0.05
+
+class Rarity(Enum):
     WHITE = 0.5
     BLUE = 0.3
     PURPLE = 0.15
@@ -18,7 +25,7 @@ class Rarity:
 class ItemData:
     """和ability差不多"""
 
-    id: int
+    id: int 
     # 招式名
     name: str
     name_cn: str
@@ -30,16 +37,16 @@ class ItemData:
     # 是否是消耗品
     consumable: bool = False
     # 一些布尔型flag
-    flags: dict[str, bool] = None
+    flags: dict[str, bool] = field(default_factory=dict)
+    
+    add_non_events: NonEventsObj = field(default_factory=NonEventsObj)
 
-    add_non_events: NonEventsObj = None
-
-    # TODO
-    def __post_init__(self):
-        if self.flags is None:
-            self.flags = {}
-        if self.add_non_events is None:
-            self.add_non_events = NonEventsObj()
+    # # TODO
+    # def __post_init__(self):
+    #     if self.flags is None:
+    #         self.flags = {}
+    #     if self.add_non_events is None:
+    #         self.add_non_events = NonEventsObj()
 
     def __str__(self) -> str:
         return self.name_cn
